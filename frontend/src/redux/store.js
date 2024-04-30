d45a60d516fcf0d63db28c7b955e72e8054b1597
saivardhanbabu <saivardhanbabu1@gmail.com>
2024-04-30 15:54:26 +0530

    added changes in store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userLoginReducer from './slices/userLoginSlice'
 import userTransferSlice from './slices/userTransferSlice';
+import React from 'react'
+
 export const reduxStore=configureStore({
     reducer:{
         userLogin:userLoginReducer,
